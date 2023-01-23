471bb4e07093a17b2729031d35010c7bf3c9c031
Nicolas-Delahaie <nico601.delahaie@gmail.com>
2023-01-23 16:59:43 +0100

    TP1

---
+++ TD1_todoList/app/src/main/java/com/example/todolist/NouvelleCategorieActivity.java
@@ -0,0 +1,14 @@
+package com.example.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NouvelleCategorieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nouvelle_categorie);
+    }
+}
--- TP1_Commandes_restaurant/app/src/main/java/com/example/tp1_commandes_restaurant/MainActivity.java
+++ TP1_Commandes_restaurant/app/src/main/java/com/example/tp1_commandes_restaurant/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.tp1_commandes_restaurant;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
